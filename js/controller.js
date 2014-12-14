94167fb0451e7ae1341e7506d20ce4421e49ba59
tuliofaria <tuliofaria@gmail.com>
2014-12-14 18:27:57 -0200

    Added: - Options:    - autoplay    - show controls. - Get current time with model

--- js/controller.js
+++ js/controller.js
@@ -2,12 +2,27 @@ var con = angular.module('youtube-controller', []);
 
 con.controller('youtube-controller', ['$scope', function($scope, youtubePlayer){
 
-	$scope.videos = ['5NV6Rdv1a3I', 'BnigNbYVQJE', 'AEoh-fPXMqc'];
+	$scope.videos = [
+		{
+			id: '5NV6Rdv1a3I',
+			showControls: 0,
+			autoplay: 1
+		}, 
+		{
+			id: 'BnigNbYVQJE',
+			showControls: 1,
+			autoplay: 0
+		}, 
+		{
+			id: 'AEoh-fPXMqc',
+			showControls: 0,
+			autoplay: 1
+		}];
 	$scope.current = $scope.videos[0];
 	$scope.index = 0;
 
 	$scope.videoID = function(){
-		return $scope.current;
+		return $scope.current.id;
 	}
 
 	$scope.nextVideo = function(){
@@ -23,14 +38,22 @@ con.controller('youtube-controller', ['$scope', function($scope, youtubePlayer){
 	}
 
 	$scope.add = '';
+	$scope.showControls = 0;
+	$scope.autoplay = 0;
 	$scope.addvideo = function(){
-		$scope.videos.push($scope.add);
-		console.log($scope.add);
+		$scope.videos.push({ 
+			id: $scope.add,
+			showControls: $scope.showControls,
+			autoplay: $scope.autoplay
+		});
+	}
+	$scope.remove = function(video){
+		$scope.videos.splice($scope.videos.indexOf(video), 1);
 	}
 
 
 	$scope.code = function(){
-		return '<youtube id="' + $scope.current + '" width="' + $scope.width + '" height="' + $scope.height + '"></youtube>';
+		return '<youtube id="' + $scope.current.id + '" width="' + $scope.width + '" height="' + $scope.height + '" controls="' + $scope.current.showControls + '"></youtube>';
 	}
 
 }]);
--- js/directive.js
+++ js/directive.js
@@ -1,35 +1,60 @@
 var dir = angular.module('youtube-directive', ['youtube-service']);
 
-dir.directive('youtube', ['youtubeEmbed', '$window', function(youtubeEmbed, $window){
+dir.directive('youtube', ['youtubeEmbed', '$window', '$interval', function(youtubeEmbed, $window, $interval){
 	return {
 		restrict: 'E',
 		template: '<div id="player"></div>',
+		scope: {
+				state: '=',
+				currentTime: '='
+		},
 		link: function(scope, element, attrs){
+			//scope.playerState = 
+			scope.currentTime = 0;
 			youtubeEmbed.yt().then(function(yt){
 				$window.onYouTubePlayerAPIReady = function(){
-					scope.player = new YT.Player('player', {
-						height: attrs.height,
-						width: attrs.width,
-						videoId: attrs.id
-					});					
-
 					scope.createPlayer = function(attrs){
-						if(scope.player) scope.player.destroy();
-						return new YT.Player('player', {
+						if(scope.player) 
+							scope.player.destroy();
+						var controls = (attrs.controls) ? attrs.controls : 1;
+						var autoplay = (attrs.autoplay) ? attrs.autoplay : 0;
+						var player = new YT.Player('player', {
 							height: attrs.height,
 							width: attrs.width,
-							videoId: attrs.id
+							videoId: attrs.id,
+							playerVars: { 'autoplay': autoplay, 'controls': controls },
 						});
+						player.addEventListener("onStateChange", function(state){
+							console.log("state", state);
+							if(state.data==1){
+								scope.timer = $interval(function(){
+									if(scope.player)
+									scope.currentTime = scope.player.getCurrentTime();
+								}, 250);
+							}else{
+								if(scope.timer){
+									$interval.cancel(scope.timer);
+								}
+							}
+						});
+						return player;
 					}
+					scope.player = scope.createPlayer(attrs);
+
+					scope.$watch(function(){ return attrs.id;}, function(newVal){
+						var videoId = newVal;
+						scope.player = scope.createPlayer(attrs);
+					});
+
+					scope.$on('$destroy', function() {
+			          // Make sure that the interval is destroyed too
+			          if(scope.timer){
+			          	$interval.cancel(scope.timer);
+			          	scope.timer = null;
+			          }
+			        });
 
 				}
-
-				scope.$watch(function(){ return attrs.id;}, function(newVal){
-						var videoId = newVal;
-						console.log(videoId);
-						scope.player = scope.createPlayer(attrs);
-				});
-
 			});
 		}
 	};
